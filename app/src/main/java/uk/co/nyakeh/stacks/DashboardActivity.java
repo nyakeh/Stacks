1fac3d558cd5f81399995799bcd0e7e06a3b8c6e
Nyakeh Rogers <nyakeh123@gmail.com>
2016-09-08 22:38:47 +0100

    + Populated Dashboard with real-time figures from the LSE.

--- app/src/main/java/uk/co/nyakeh/stacks/DashboardActivity.java
+++ app/src/main/java/uk/co/nyakeh/stacks/DashboardActivity.java
@@ -1,6 +1,7 @@
 package uk.co.nyakeh.stacks;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -9,8 +10,23 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class DashboardActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private TextView mDiff;
+    private TextView mPercentageChange;
+    private TextView mPortfolio;
+    private TextView mPercentageFI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +34,63 @@ public class DashboardActivity extends AppCompatActivity implements NavigationVi
         setContentView(R.layout.activity_dashboard);
         Toolbar toolbar = (Toolbar) findViewById(R.id.dashboard_toolbar);
         setSupportActionBar(toolbar);
+        SetupNavigation(toolbar);
 
+        mDiff = (TextView) findViewById(R.id.dashboard_diff);
+        mPercentageChange = (TextView) findViewById(R.id.dashboard_percentageChange);
+        mPortfolio = (TextView) findViewById(R.id.dashboard_portfolio);
+        mPercentageFI = (TextView) findViewById(R.id.dashboard_percentageFI);
+        new WebClient().execute("http://finance.google.com/finance/info?client=ig&q=LON:VMID");
+    }
+
+    public class WebClient extends AsyncTask<String, Integer, String> {
+        private OkHttpClient mOkHttpClient;
+
+        public WebClient() {
+            mOkHttpClient = new OkHttpClient();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            String result = null;
+            String url = params[0];
+            Request request = new Request.Builder()
+                    .url(url)
+                    .build();
+
+            try {
+                Response response = mOkHttpClient.newCall(request).execute();
+                result = response.body().string();
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            try {
+                JSONObject share = new JSONObject(result);
+                double sharePrice = Double.parseDouble(share.get("l").toString());
+                double portfolioSum = sharePrice * 112;
+                int fi_target = 420000;
+                double purchaseSum = 3062.87;
+                double diff = portfolioSum - purchaseSum;
+                double percentageChange = (diff / purchaseSum)*100;
+                double percentageFI = portfolioSum / fi_target;
+
+                mDiff.setText(getString(R.string.money_format, diff));
+                mPercentageChange.setText(getString(R.string.percentage_format, percentageChange));
+                mPortfolio.setText(getString(R.string.money_format, portfolioSum));
+                mPercentageFI.setText(getString(R.string.format_4dp, percentageFI));
+            } catch (JSONException exception) {
+                exception.printStackTrace();
+            }
+        }
+    }
+
+    private void SetupNavigation(Toolbar toolbar) {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.dashboard_drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
