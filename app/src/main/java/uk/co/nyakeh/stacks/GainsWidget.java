ca70d5fd1b89cbc4730de6cab5f7d6a837d78ba8
Nyakeh Rogers <nyakeh123@gmail.com>
2016-09-21 21:36:53 +0100

    + Perform a lookup & calculate the percentage gains of my portfolio before updating the widget with the correct value.

--- app/src/main/java/uk/co/nyakeh/stacks/GainsWidget.java
+++ app/src/main/java/uk/co/nyakeh/stacks/GainsWidget.java
@@ -3,17 +3,19 @@ package uk.co.nyakeh.stacks;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.widget.RemoteViews;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class GainsWidget extends AppWidgetProvider {
-
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
-        CharSequence widgetText = context.getString(R.string.appwidget_text, 5.64);
-
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.gains_widget);
-        views.setTextViewText(R.id.gains_widget_text, widgetText);
-        appWidgetManager.updateAppWidget(appWidgetId, views);
-    }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
@@ -22,9 +24,56 @@ public class GainsWidget extends AppWidgetProvider {
         }
     }
 
-    @Override
-    public void onEnabled(Context context) {
-        // Enter relevant functionality for when the first widget is created
+    void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.gains_widget);
+        WebClient latestSharePriceTask = new WebClient(views, appWidgetManager, appWidgetId);
+        latestSharePriceTask.execute("http://finance.google.com/finance/info?client=ig&q=LON:VMID");
+    }
+
+    public class WebClient extends AsyncTask<String, Integer, String> {
+        private OkHttpClient mOkHttpClient = new OkHttpClient();
+        private RemoteViews mViews;
+        private AppWidgetManager mAppWidgetManager;
+        private int mAppWidgetId;
+
+        public WebClient(RemoteViews views, AppWidgetManager appWidgetManager, int appWidgetId) {
+            mViews = views;
+            mAppWidgetManager = appWidgetManager;
+            mAppWidgetId = appWidgetId;
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            String result = null;
+            String url = params[0];
+            Request request = new Request.Builder().url(url).build();
+            try {
+                Response response = mOkHttpClient.newCall(request).execute();
+                result = response.body().string();
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String responseBody) {
+            super.onPostExecute(responseBody);
+            try {
+                JSONObject share = new JSONObject(responseBody);
+                double sharePrice = Double.parseDouble(share.get("l").toString());
+                double portfolioSum = sharePrice * 112;
+                double purchaseSum = 3062.87;
+                double diff = portfolioSum - purchaseSum;
+                double percentageChange = (diff / purchaseSum) * 100;
+                CharSequence widgetText = String.format("↑ %.2f%%", percentageChange);
+
+                mViews.setTextViewText(R.id.gains_widget_text, widgetText);
+                mAppWidgetManager.updateAppWidget(mAppWidgetId, mViews);
+            } catch (JSONException exception) {
+                exception.printStackTrace();
+            }
+        }
     }
 }
 
