d4a72fdf836e0fe426829037b01325b43c378b4e
Nyakeh Rogers <nyakeh123@gmail.com>
2016-11-26 14:08:34 +0000

    + Widget no longer used hardcoded values for the investment portfolio.

--- app/src/main/java/uk/co/nyakeh/stacks/GainsWidget.java
+++ app/src/main/java/uk/co/nyakeh/stacks/GainsWidget.java
@@ -8,10 +8,15 @@ import android.widget.RemoteViews;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.List;
+
+import uk.co.nyakeh.stacks.database.StockLab;
+import uk.co.nyakeh.stacks.objects.StockPurchase;
+
 public class GainsWidget extends AppWidgetProvider implements IAsyncTask {
-    private RemoteViews mViews;
-    private AppWidgetManager mAppWidgetManager;
-    private int mAppWidgetId;
+    private AppWidgetManager _appWidgetManager;
+    private int _appWidgetId;
+    private Context _context;
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
@@ -21,27 +26,34 @@ public class GainsWidget extends AppWidgetProvider implements IAsyncTask {
     }
 
     void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
-        mViews = new RemoteViews(context.getPackageName(), R.layout.gains_widget);
-        mAppWidgetManager = appWidgetManager;
-        mAppWidgetId = appWidgetId;
+        _context = context;
+        _appWidgetManager = appWidgetManager;
+        _appWidgetId = appWidgetId;
         new GoogleFinanceClient(this).execute("LON:VMID");
     }
 
     @Override
     public void PostExecute(String response) {
+        RemoteViews layout = new RemoteViews(_context.getPackageName(), R.layout.gains_widget);
         try {
             JSONObject share = new JSONObject(response);
             double sharePrice = Double.parseDouble(share.get("l").toString());
-            double portfolioSum = sharePrice * 112;
-            double purchaseSum = 3062.87;
-            double diff = portfolioSum - purchaseSum;
-            double percentageChange = (diff / purchaseSum) * 100;
+            double purchaseCost = 0;
+            int stockQuantity = 0;
+            List<StockPurchase> stockPurchaseHistory = StockLab.get(_context).getStockPurchaseHistory("VMID.L");
+            for (StockPurchase stockPurchase : stockPurchaseHistory) {
+                purchaseCost += stockPurchase.Total;
+                stockQuantity += stockPurchase.Quantity;
+            }
+            double currentPortfolioValue = sharePrice * stockQuantity;
+            double diff = currentPortfolioValue - purchaseCost;
+            double percentageChange = (diff / purchaseCost) * 100;
             CharSequence formattedDiff = String.format("£%,.2f", diff);
             CharSequence formattedPercentageChange = String.format("↑%.2f%%", percentageChange);
 
-            mViews.setTextViewText(R.id.gains_widget_diff, formattedDiff);
-            mViews.setTextViewText(R.id.gains_widget_percentage, formattedPercentageChange);
-            mAppWidgetManager.updateAppWidget(mAppWidgetId, mViews);
+            layout.setTextViewText(R.id.gains_widget_diff, formattedDiff);
+            layout.setTextViewText(R.id.gains_widget_percentage, formattedPercentageChange);
+            _appWidgetManager.updateAppWidget(_appWidgetId, layout);
         } catch (JSONException exception) {
             exception.printStackTrace();
         }
